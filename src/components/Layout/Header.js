29653c52940efb8f2a200ca3ff7601cc13cf1a01
Per Kristian Willand Engebretsen <perkristianwilland@Pers-MacBook-Pro.local>
2021-09-11 11:27:46 +0200

    Navigation is now responsive

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,13 @@
-import { React, useContext } from "react";
+import { React, useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { AiOutlineUser } from "react-icons/ai";
 import { IoLogOutOutline } from "react-icons/io5";
 import AuthContext from "../../context/AuthContext";
+import { RiMenu4Line, RiCloseLine } from "react-icons/ri"
 
 export default function Header({ handleClick }) {
   const [auth, setAuth] = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
 
   const history = useHistory();
 
@@ -17,24 +19,28 @@ export default function Header({ handleClick }) {
   return (
     <div className="header">
       {/* Erstatt med logo */}
-      <Link to="/">
+      <Link to="/" onClick={() => setOpen(false)}>
         <h1 className="header__logo">Holidaze</h1>
       </Link>
-      <nav>
-        <ul className="header__links">
-          <Link exact to="/">
-            <li className="header__item">Home</li>
-          </Link>
-          <Link to="/places">
-            <li className="header__item">Places</li>
-          </Link>
-          <Link to="/contact">
-            <li className="header__item">Contact</li>
-          </Link>
+      
+        <div className="header__hamburger" onClick={() => setOpen(!open)}>
+          {open? <RiCloseLine/> : <RiMenu4Line />}
+        </div>
+        <nav>
+          <ul className={open ? "header__links--open" : "header__links"}>
+            <Link exact to="/" onClick={() => setOpen(false)}>
+              <li className="header__item">Home</li>
+            </Link>
+            <Link to="/places" onClick={() => setOpen(false)}>
+              <li className="header__item">Places</li>
+            </Link>
+            <Link to="/contact" onClick={() => setOpen(false)}>
+              <li className="header__item">Contact</li>
+            </Link>
 
           {auth ? (
             <>
-              <Link to="/dashboard">
+              <Link to="/dashboard" onClick={() => setOpen(false)}>
                 <li className="header__item">Dashboard</li>
               </Link>
 
@@ -51,7 +57,7 @@ export default function Header({ handleClick }) {
               </button>
             </li>
           )}
-        </ul>
+         </ul>
       </nav>
     </div>
   );
--- src/pages/HotelDetails.js
+++ src/pages/HotelDetails.js
@@ -2,10 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 import Loader from "../components/Layout/Loader";
+import { BASE_URL } from "../constants/api";
 
 const HOTEL = gql`
   query getHotel($id: ID!) {
-    hotel(id: $id) {
+    place(id: $id) {
       title
       description
       image {
@@ -31,17 +32,17 @@ export default function HotelDetails() {
   return (
     <div className="hotel-details">
       <img
-        src={"http://localhost:1337" + data.hotel.image.url}
-        alt={"http://localhost:1337" + data.hotel.image.alternativeText}
+        src={BASE_URL + data.place.image.url}
+        alt={BASE_URL + data.place.image.alternativeText}
       />
       <div className="hotel-card__content">
-        <h2>{data.hotel.title}</h2>
-        <p className="description">{data.hotel.description}</p>
+        <h2>{data.place.title}</h2>
+        <p className="description">{data.place.description}</p>
         <div className="price">
           <span>From </span>
-          <span className="price-number">{data.hotel.price} NOK</span>
+          <span className="price-number">{data.place.price} NOK</span>
         </div>
-        <div className="rating">{data.hotel.rating}</div>
+        <div className="rating">{data.place.rating}</div>
       </div>
     </div>
   );
