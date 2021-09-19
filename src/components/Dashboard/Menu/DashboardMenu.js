2fdfce818fd36ef1c8a7541ca074b3e1c3326325
Per Kristian <per.kristian@creatur.no>
2021-09-19 11:30:04 +0200

    Messages now have a functional accordion/dropdown to view full message

--- src/components/Dashboard/Menu/DashboardMenu.js
+++ src/components/Dashboard/Menu/DashboardMenu.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AiFillHome } from "react-icons/ai"
 import { IoMdAddCircle } from "react-icons/io"
 import { FaInbox } from "react-icons/fa"
 import {RiHotelFill } from "react-icons/ri"
+import AuthContext from '../../../context/AuthContext'
+import Avatar from "../../../assets/svg/avatar.svg";
 
 
 export default function DashboardMenu() {
+  const [auth] = useContext(AuthContext);
   return (
     <div className="dashboard__menu">
+      <img src={Avatar} alt="Standard avatar illustration" />
+      <h2>{auth.user.username}</h2>
+      <h3>{auth.user.email}</h3>
       <nav>
         <ul>
         <Link to="/dashboard">
--- src/components/Dashboard/Messages.js
+++ src/components/Dashboard/Messages.js
@@ -3,12 +3,21 @@ import { BASE_URL, MESSAGES } from "../../constants/api";
 import useAxios from '../../hooks/useAxios';
 import moment from 'moment';
 import { motion } from 'framer-motion';
+import { GoChevronDown, GoChevronUp } from "react-icons/go"
 
 const url = BASE_URL + MESSAGES;
 
 export default function Messages() {
   const http = useAxios();
   const [messages, setMessages] = useState([]);
+  const [clicked, setClicked] = useState(false)
+
+  const toggle = (index) => {
+    if(clicked === index) {
+        return setClicked(null)
+    }
+    setClicked(index)
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -21,20 +30,33 @@ export default function Messages() {
     }; fetchData(); 
 }, []);
 
+    // This should ideally be sorted by date, but it works
+    const newFirst = [...messages].reverse();
+    console.log(newFirst)
+
     return (
       <>
+      <h1>Messages</h1>
       <div className="messages">
-        {messages.map((message) => (
+        {newFirst.map((message, index) => (
             <motion.div
-            key={message.id}
+            key={index}
             className="message-card"
-            whileHover={{ scale: 1.07 }}
+            whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
+            onClick={() => toggle(index)}
             >
-            
-                <h2 className="message-card__name">{message.Name}</h2>
+                <h2 className="message-card__name">
+                    {message.Name}
+                    <span>{clicked === index ? <GoChevronUp /> : <GoChevronDown />}</span>
+                </h2>
                 <h3 className="message-card__date">{moment(message.published_at).format("DD.MM.YYYY - HH:mm")}</h3>
-                <p className="message-card__excerpt">{message.Content}</p>
+                {clicked === index ? (
+                    <div className="full-message">
+                        <h4>From: {message.Email}</h4>
+                        <p>{message.Content}</p>
+                    </div>
+                ) : <p className="message-card__excerpt">{`${message.Content}`.substring(0, 40)}...</p>}
             </motion.div>
           ))}
       </div>
