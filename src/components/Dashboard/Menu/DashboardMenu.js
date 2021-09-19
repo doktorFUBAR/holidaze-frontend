5cb73db42d54dc44661cf32f450ab50d466d7ccb
Per Kristian <per.kristian@creatur.no>
2021-09-19 15:43:21 +0200

    Added amenities to hotel details page

--- src/components/Dashboard/Menu/DashboardMenu.js
+++ src/components/Dashboard/Menu/DashboardMenu.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AiFillHome } from "react-icons/ai"
 import { IoMdAddCircle } from "react-icons/io"
@@ -6,10 +6,32 @@ import { FaInbox } from "react-icons/fa"
 import {RiHotelFill } from "react-icons/ri"
 import AuthContext from '../../../context/AuthContext'
 import Avatar from "../../../assets/svg/avatar.svg";
+import { BASE_URL, MESSAGES } from '../../../constants/api'
+import useAxios from '../../../hooks/useAxios'
+
+
+const url = BASE_URL + MESSAGES;
+
+
 
 
 export default function DashboardMenu() {
+  const http = useAxios();
   const [auth] = useContext(AuthContext);
+  const [messages, setMessages] = useState(null)
+
+  const getMessages = async () => {
+    try {
+      const res = await http.get(url);
+      setMessages(res.data.length)
+      console.log(messages)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  getMessages();
+
   return (
     <div className="dashboard__menu">
       <img src={Avatar} alt="Standard avatar illustration" />
@@ -24,7 +46,7 @@ export default function DashboardMenu() {
           <li><IoMdAddCircle /> Add hotel</li>
         </Link>
         <Link to="/dashboard/messages">
-          <li><FaInbox /> Messages</li>
+          <li><FaInbox /> Messages <span className="notifcation">{messages}</span></li>
         </Link>
         <Link to="/dashboard/booking-requests">
           <li><RiHotelFill /> Booking</li>
--- src/pages/HotelDetails.js
+++ src/pages/HotelDetails.js
@@ -4,6 +4,9 @@ import { useQuery, gql } from "@apollo/client";
 import Loader from "../components/Layout/Loader";
 import Button from "../components/Common/Button";
 import { GiRoundStar } from "react-icons/gi"
+import { TiArrowBackOutline } from "react-icons/ti"
+import { AiOutlineWifi, AiFillClockCircle } from "react-icons/ai"
+import { MdFreeBreakfast } from "react-icons/md"
 
 const HOTEL = gql`
   query getHotel($id: ID!) {
@@ -31,7 +34,11 @@ export default function HotelDetails() {
   if (error) return <p>Error...</p>;
 
   return (
+    <>
     <div className="hotel-details">
+    <Link to="/places">
+          <Button text={<TiArrowBackOutline />} />
+      </Link>
       <img
         src={data.place.Image[0].url}
         alt={data.place.Image[0].alternativeText}
@@ -40,6 +47,15 @@ export default function HotelDetails() {
       <div className="hotel-card__content">
         <h2>{data.place.Title}</h2>
         <p className="hotel-details__description">{data.place.Description}</p>
+        <article className="hotel-details__amenities">
+        <div className="hotel-details__col">
+          <AiFillClockCircle />
+          <div>Check in: 15:00</div>
+          <div>Check out: 12:00</div>
+          </div>
+        <div className="hotel-details__col"><AiOutlineWifi/> Free Wifi</div>
+        <div className="hotel-details__col"><MdFreeBreakfast/> Breakfast included</div>
+        </article>
         <div className="hotel-details__price">
           <span>From </span>
           <span className="price-number">{data.place.Price} NOK</span>
@@ -50,5 +66,7 @@ export default function HotelDetails() {
         </Link>
       </div>
     </div>
+
+    </>
   );
 }
