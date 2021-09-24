a6103ad6f803b838d0dff7517a9011d9d94e1822
Per Kristian <per.kristian@creatur.no>
2021-09-24 18:25:06 +0200

    Did some changes to the rating badge and hotel details styles

--- src/components/Homepage/FeaturedPlaces.js
+++ src/components/Homepage/FeaturedPlaces.js
@@ -34,10 +34,13 @@ export default function FeaturedPlaces() {
         {data.category.places.map((place) => (
           <Link to={`/details/${place.id}`}>
             <div key={place.id} className="hotel-card">
+            <div className="hotel-card__image-wrapper">
               <img
                 src={place.Image[0].url}
                 alt={place.Image[0].alternativeText}
               />
+               <div className="rating"><span className="rating__icon"><GiRoundStar/></span>{place.Rating}</div>
+            </div>
               <div className="hotel-card__content">
                 <h2>{place.Title}</h2>
                 <div className="price">
@@ -45,7 +48,6 @@ export default function FeaturedPlaces() {
                   <span className="price-number">{place.Price} NOK</span>
                 </div>
                 <div className="hotel-card__bottom">
-                  <div className="rating"><span className="rating__icon"><GiRoundStar/></span>{place.Rating}</div>
                   <div className="featured-badge">Popular</div>
                 </div>
               </div>
--- src/components/Hotels/HotelCollection.js
+++ src/components/Hotels/HotelCollection.js
@@ -16,17 +16,20 @@ export default function HotelCollection() {
         {data.map((place) => (
           <Link to={`/details/${place.id}`}>
           <div key={place.id} className="hotel-card">
-            <img
-              src={place.Image[0].url}
-              alt={place.Image[0].alternativeText}
-            />
+            <div className="hotel-card__image-wrapper">
+              <img
+                src={place.Image[0].url}
+                alt={place.Image[0].alternativeText}
+              />
+               <div className="rating"><span className="rating__icon"><GiRoundStar/></span>{place.Rating}</div>
+            </div>
             <div className="hotel-card__content">
               <h2>{place.Title}</h2>
               <div className="price">
                 <span>From </span>
                 <span className="price-number">{place.Price} NOK</span>
               </div>
-              <div className="rating"><span className="rating__icon"><GiRoundStar/></span>{place.Rating}</div>
+             
             </div>
           </div>
           </Link>
