335bf29d9a2132f66c71ea5f116975511a384855
Per Kristian <per.kristian@creatur.no>
2021-09-24 21:58:36 +0200

    Further styling on the dashboard. Added a dropdown for add hotel

--- src/components/Dashboard/DashboardHome.js
+++ src/components/Dashboard/DashboardHome.js
@@ -5,16 +5,17 @@ import Statistics from './Statistics';
 import Enquiries from "./Enquiries";
 import Messages from "./Messages";
 import AddHotel from "./AddHotel";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
 export default function DashboardHome() {
   const [showAdd, setshowAdd] = useState(false);
 
   return (
-    <div>
+    <div className="dashboard">
       <Heading text="Welcome to the Holidaze Dashboard!" />
       <Statistics />
       <HotelCollection />
-      <p onClick={() => setshowAdd(!showAdd)}>Add new hotel!</p>
+      <h3 className="dashboard__dropdown" onClick={() => setshowAdd(!showAdd)}>Add new hotel! {!showAdd ? <GoChevronDown /> : <GoChevronUp />}</h3>
       {showAdd  ? <AddHotel /> : null}
       <div className="dashboard__dual">
         <div className="dashboard__col">
