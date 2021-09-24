eaf9ac9213ef5eeb14c7502a3956e7ae7c90e415
Per Kristian <per.kristian@creatur.no>
2021-09-24 21:35:17 +0200

    Added _redirects

--- src/components/Dashboard/DashboardHome.js
+++ src/components/Dashboard/DashboardHome.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../Common/Heading'
 import HotelCollection from "../Hotels/HotelCollection";
 import Statistics from './Statistics';
 import Enquiries from "./Enquiries";
 import Messages from "./Messages";
+import AddHotel from "./AddHotel";
 
 export default function DashboardHome() {
+  const [showAdd, setshowAdd] = useState(false);
+
   return (
     <div>
       <Heading text="Welcome to the Holidaze Dashboard!" />
       <Statistics />
       <HotelCollection />
+      <p onClick={() => setshowAdd(!showAdd)}>Add new hotel!</p>
+      {showAdd  ? <AddHotel /> : null}
       <div className="dashboard__dual">
         <div className="dashboard__col">
           <Heading text="Booking enquires" />
